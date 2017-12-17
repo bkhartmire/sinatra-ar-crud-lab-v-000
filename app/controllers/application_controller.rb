0f42107816f5ceef8d3fa1f8fa94b6eefdefb061
Brittany Hartmire <bkhartmire@gmail.com>
2017-12-17 15:56:02 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,7 +34,7 @@ class ApplicationController < Sinatra::Base
   patch '/posts/:id' do
     @post = Post.find(params[:id])
     @post.update(name: params[:name], content: params[:content])
-    erb :show
+    redirect '/posts/:id'
   end
 
   delete '/posts/:id/delete' do
